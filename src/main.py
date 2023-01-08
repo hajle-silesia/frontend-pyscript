f4c7cb05b94b99ae83fd17bc7d5a76c5ec29cb29
mtweeman <mtweeman@gmail.com>
2023-01-08 18:16:31 +0100

    Moved elements init to main function

--- src/main.py
+++ src/main.py
@@ -26,29 +26,35 @@ def show(*args, **kwargs):
 brewery_bg = "./img/brewery_bg.png"
 button_off = "./img/button_off.png"
 button_on = "./img/button_on.png"
-document.getElementById("brewery_bg").setAttribute("src", brewery_bg)
-document.getElementById("pump_1").setAttribute("src", button_off)
-document.getElementById("pump_2").setAttribute("src", button_off)
-document.getElementById("pump_3").setAttribute("src", button_off)
-document.getElementById("bk_drain").setAttribute("src", button_off)
-document.getElementById("bk_wort").setAttribute("src", button_off)
-document.getElementById("bk_whirlpool").setAttribute("src", button_off)
-document.getElementById("pump_1_cfc").setAttribute("src", button_off)
-document.getElementById("pump_1_wort_out").setAttribute("src", button_off)
-document.getElementById("pump_1_drain").setAttribute("src", button_off)
-document.getElementById("cfc_drain").setAttribute("src", button_off)
-document.getElementById("water_in_cfc").setAttribute("src", button_off)
-document.getElementById("pump_2_pump_1").setAttribute("src", button_off)
-document.getElementById("pump_2_rims").setAttribute("src", button_off)
-document.getElementById("mlt_recirculation").setAttribute("src", button_off)
-document.getElementById("water_in_pump_3").setAttribute("src", button_off)
-document.getElementById("pump_3_hlt").setAttribute("src", button_off)
-document.getElementById("pump_3_rims").setAttribute("src", button_off)
-document.getElementById("hlt_drain").setAttribute("src", button_off)
-document.getElementById("bk_cip").setAttribute("src", button_off)
-document.getElementById("mlt_cip").setAttribute("src", button_off)
-document.getElementById("mlt_sightglass").setAttribute("src", button_off)
-document.getElementById("mlt_sightglass_cam").setAttribute("src", button_off)
-document.getElementById("mlt_sightglass_size").setAttribute("src", button_off)
 
-document.getElementById("recipe").hidden = True
+
+def main():
+    document.getElementById("brewery_bg").setAttribute("src", brewery_bg)
+    document.getElementById("pump_1").setAttribute("src", button_off)
+    document.getElementById("pump_2").setAttribute("src", button_off)
+    document.getElementById("pump_3").setAttribute("src", button_off)
+    document.getElementById("bk_drain").setAttribute("src", button_off)
+    document.getElementById("bk_wort").setAttribute("src", button_off)
+    document.getElementById("bk_whirlpool").setAttribute("src", button_off)
+    document.getElementById("pump_1_cfc").setAttribute("src", button_off)
+    document.getElementById("pump_1_wort_out").setAttribute("src", button_off)
+    document.getElementById("pump_1_drain").setAttribute("src", button_off)
+    document.getElementById("cfc_drain").setAttribute("src", button_off)
+    document.getElementById("water_in_cfc").setAttribute("src", button_off)
+    document.getElementById("pump_2_pump_1").setAttribute("src", button_off)
+    document.getElementById("pump_2_rims").setAttribute("src", button_off)
+    document.getElementById("mlt_recirculation").setAttribute("src", button_off)
+    document.getElementById("water_in_pump_3").setAttribute("src", button_off)
+    document.getElementById("pump_3_hlt").setAttribute("src", button_off)
+    document.getElementById("pump_3_rims").setAttribute("src", button_off)
+    document.getElementById("hlt_drain").setAttribute("src", button_off)
+    document.getElementById("bk_cip").setAttribute("src", button_off)
+    document.getElementById("mlt_cip").setAttribute("src", button_off)
+    document.getElementById("mlt_sightglass").setAttribute("src", button_off)
+    document.getElementById("mlt_sightglass_cam").setAttribute("src", button_off)
+    document.getElementById("mlt_sightglass_size").setAttribute("src", button_off)
+
+    document.getElementById("recipe").hidden = True
+
+
+main()
